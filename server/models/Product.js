f4542e274836e7bc85e65412e416d36957e5981d
SheKillinIt <spradlingsheila1991@gmail.com>
2026-06-21 05:28:21 -0500

    Create Product model

---
+++ server/models/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true
+  },
+  image: String,
+  quantity: {
+    type: Number,
+    default: 0
+  },
+  category: String,
+  featured: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
